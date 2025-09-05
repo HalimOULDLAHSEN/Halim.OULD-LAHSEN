a8ba3048774962a47b2e1feed45bc2648febf23b
HalimOULDLAHSEN <halim.ould.lahsen@gmail.com>
2025-09-06 00:47:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 document.getElementById("contact-form").addEventListener("submit", function(e) {
   e.preventDefault();
+
   const userName = document.getElementById("user-name").value;
   const userEmail = document.getElementById("user-email").value;
 
-  // ✅ 1. Envoi automatique au visiteur (ton template actuel)
+  // Envoi au visiteur
   emailjs.send("service_b0bun1l", "template_5wek4qv", {
     email: userEmail
   }, "E2nceqWyEUuq_Yntx");
-// ✅ 2. Notification envoyée à toi (il faut créer un template exprès dans EmailJS)
+
+  // Notification à toi-même
   emailjs.send("service_b0bun1l", "template_qbmrgpx", {
-  name: userName,
-  email: userEmail
-}, "E2nceqWyEUuq_Yntx")
+    name: userName,
+    email: userEmail
+  }, "E2nceqWyEUuq_Yntx")
   .then(() => {
     document.getElementById("confirmation").style.display = "block";
     document.getElementById("contact-form").reset();
@@ -19,9 +21,3 @@ document.getElementById("contact-form").addEventListener("submit", function(e) {
     alert("Erreur lors de l’envoi : " + JSON.stringify(error));
   });
 });
-
-
-emailjs.send("service_b0bun1l", "template_notify_me", {
-  name: userName,
-  email: userEmail
-}, "E2nceqWyEUuq_Yntx")
